4b923ac7226c411e395f6a5d8f288b2e759e5698
Chris Johnson <chris.johnson@traka.com>
2023-08-17 15:29:30 +0100

    Solved Added tests for multiple aces and a losing hand

--- src/main.rs
+++ src/main.rs
@@ -30,8 +30,36 @@ impl Hand {
     }
 
     fn value(&self) -> usize {
-        // TODO: implement this method
-        0 
+        let mut value:usize = 0;
+        let mut high_aces:usize = 0;
+        for card in self.cards.iter() {
+            match card {
+                Card::Ace if value > 10 => value += 1,
+                Card::Ace if high_aces > 0 && value > 10 => value -= 10,
+                Card::Ace => {
+                    value += 11;
+                    high_aces += 1;
+                },
+                Card::Two => value += 2,
+                Card::Three => value += 3,
+                Card::Four => value += 4,
+                Card::Five => value += 5,
+                Card::Six => value += 6,
+                Card::Seven => value += 7,
+                Card::Eight => value += 8,
+                Card::Nine => value += 9,
+                Card::Jack => value += 10,
+                Card::Queen => value += 10,
+                Card::King => value += 10,
+            }
+        }
+        if value > 21{
+            while  high_aces > 0 {
+                high_aces -= 1;
+                value -= 10;
+            }
+        }
+        value
     }
 
     fn is_loosing_hand(&self) -> bool {
@@ -60,6 +88,28 @@ fn strong_hand() {
     hand.add(Card::Ace);
 
     assert_eq!(hand.value(), 21);
+}
+
+#[test]
+fn double_aces_and_queen() {
+    let mut hand = Hand::new();
+    hand.add(Card::Queen);
+    hand.add(Card::Ace);
+    hand.add(Card::Ace);
+
+    assert_eq!(hand.value(), 12);
+}
+
+#[test]
+fn double_aces_and_double_kings() {
+    let mut hand = Hand::new();
+    hand.add(Card::King);
+    hand.add(Card::King);
+    hand.add(Card::Ace);
+    hand.add(Card::Ace);
+
+    assert_eq!(hand.value(), 22);
+    assert_eq!(hand.is_loosing_hand(), true);
 }
 
 #[test]
