3acb77220810792448adacfc6e3cc7d23653f42d
Chris Johnson <chris.johnson@traka.com>
2023-08-17 15:34:22 +0100

    Solved

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 // TODO: import the necessary dependencies
 
+use chrono::{Local, TimeZone};
+
 struct ImportantEvent {
-    // TODO: define data structure
+    what: String,
+    when: chrono::Date<Local>,
 }
 
 trait Deadline {
@@ -9,7 +12,9 @@ trait Deadline {
 }
 
 impl Deadline for ImportantEvent {
-    // TODO: implement trait
+    fn is_passed(&self) -> bool {
+        self.when < Local::today()
+    }
 }
 
 fn main() {
